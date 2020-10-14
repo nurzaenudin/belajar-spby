7a687d476c065f67a0e4d251c8e6e2438716ef63
nur zaenudin <nurzaenudin@gmail.com>
2020-10-14 17:42:29 +0700

    pertama

---
+++ src/main/java/com/nurzaenudin/belajarspby/BelajarSpbyApplication.java
@@ -0,0 +1,13 @@
+package com.nurzaenudin.belajarspby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpbyApplication.class, args);
+	}
+
+}
